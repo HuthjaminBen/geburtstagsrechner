b07f28fda9fd2cad79a2d98c01304238b0442cb8
Huthjamin Ben <48474573+HuthjaminBen@users.noreply.github.com>
2020-11-10 15:26:06 +0100

    erste Uploads
    Bisher funktioniert nur User-login und -signin. Ich muss dringend die Passwörter hashen und salten (und pfeffern) - habe das aber nicht zum laufen gebracht bisher...

---
+++ header.php
@@ -0,0 +1,175 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <link href="css/style.css" type="text/css" rel="stylesheet" />
+    <link rel="icon" href="favicon.ico" type="image/png" />
+    <!--<script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/javascript.js" type="text/javascript"></script>-->
+    <title>Geburtstagsrechner - Ausbildungsprojekt</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="title" content="IT Benjamin Huth" />
+    <meta name="author" content="Benjamin Huth" />
+</head>
+<body>
+<header>
+        <div id="logo">
+            <a href="https://www.benjamin-huth.de" title="zur Startseite">
+                <img src="bilder/logo.png" alt="logo">
+            </a>
+        </div>
+        <nav id="navigation">
+            <ul>
+                <li>
+                    <a href="https://fachinformatiker.benjamin-huth.de" title="Fachinformatiker">
+                        <h2>Fachinformatiker</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="https://fachinformatiker.benjamin-huth.de#anwendungsentwicklung">
+                                <h3>Anwendungsentwicklung</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://fachinformatiker.benjamin-huth.de#webseiten">
+                                <h3>Webseiten gestalten</h3>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://historiker.benjamin-huth.de" title="Historiker">
+                        <h2>Historiker</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#einblick" title="ein Einblick in die Arbeit">
+                                <h3>Einblick in die Arbeit</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#historiker" title="historiker">
+                                <h3>Werdegang</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#veroeffentlichungen19" title="Referenzen 2019">
+                                <h3>Referenz 2019</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#veroeffentlichungen18" title="Referenzen 2018">
+                                <h3>Referenz 2018</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#veroeffentlichungen17" title="Referenzen 2017">
+                                <h3>Referenz 2017</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://historiker.benjamin-huth.de#veroeffentlichungen16" title="Älteres">
+                                <h3>Älteres</h3>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://theologe.benjamin-huth.de" title="Theologe">
+                        <h2>Theologe</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#naechstes">
+                                <h3>Termine</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#predigten">
+                                <h3>Predigten</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#2019">
+                                <h3>2019</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#2018">
+                                <h3>2018</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#2017">
+                                <h3>2017</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://theologe.benjamin-huth.de#aelter">
+                                <h3>älteres </h3>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- <li>
+                    <a href="https://musikerautor.benjamin-huth.de" title="Musiker">
+                        <h2>Musiker|Autor</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="https://musikerautor.benjamin-huth.de#musiker">
+                                <h3>Musiker</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://musikerautor.benjamin-huth.de#autor">
+                                <h3>Autor</h3>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://privat.benjamin-huth.de" title="Privat">
+                        <h2>Privat</h2>
+                    </a>
+                    <ul class="linksruecker">
+                        <li>
+                            <a href="https://privat.benjamin-huth.de#privat">
+                                <h3>Heemt<span class="klein">Heimat</span></h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://privat.benjamin-huth.de#klima">
+                                <h3>Klimawandel</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://privat.benjamin-huth.de#laufen">
+                                <h3>laufen</h3>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://privat.benjamin-huth.de#undsonst" alt="ceterum censeo">
+                                <h3>darüber hinaus</h3>
+                            </a>
+                        </li>
+                    </ul>
+                </li>-->
+            </ul>
+            <div>
+
+           
+               
+            </div>
+        </nav>
+        <div id="smartphone">
+            <a href="#bitte_scrollen">
+                <img src="bilder/menubutton.png">
+            </a>
+        </div>
+    </header>
---
+++ signup.php
@@ -0,0 +1,61 @@
+<?php
+require "header.php";
+?>
+
+<main>
+<h1>Account anlegen</h1>
+<?php
+    if (isset($_GET['error'])) {
+        if($GET['error'] == "emptyfield"){
+            echo '<p class="errortext">Bitte alle Felder ausfüllen.</p>';
+        }  
+        else if($GET['error'] == "invalidusername"){
+            echo '<p class="errortext">Der Benutzername entspricht nicht den Namenskriterien.</p>';
+        }  
+        else if($GET['error'] == "invalidusernameandpassword"){
+            echo '<p class="errortext">Sowohl Benutzername als auch Email entsprechen nicht den Kriterien.</p>';
+        }  
+        else if($GET['error'] == "invalidmailadress"){
+            echo '<p class="errortext">Die Mailadresse entspricht nicht den Kriterien.</p>';
+        }  
+        else if($GET['error'] == "missmatchpasswords"){
+            echo '<p class="errortext">Der Benutzername entspricht nicht den Namenskriterien.</p>';
+        }  
+        else if($GET['error'] == "sql-error"){
+            echo '<p class="errortext">Ein Fehler in der SQL-Datenbank ist aufgetreten.</p>';
+        }  
+        else if($GET['error'] == "usernametaken"){
+            echo '<p class="errortext">Der Benutzername ist bereits vergeben.</p>';
+        } 
+    } 
+    else if($GET['signup'] == "successful"){
+        echo '<p class="successtext">Ein Nutzerkonto wurde erfolgreich erstellt.</p>';
+    }  
+?>
+
+<form action="intern/signup.intern.php" method="post">
+<input type="text" name="name_user" placeholder="Benutzername">
+<input type="text" name="mail_user" placeholder="Email">
+<input type="password" name="pwd_user" placeholder="P@§$w0rt ;)">
+<input type="password" name="pwd_user_2" placeholder="Wiederhole P@§$w0rt">
+<button type="submit" name="signup-submit">Account anlegen</button>
+
+</form>
+
+   <!-- <h1>Willkommen auf dem Geburtstagsrechner</h1>
+    <p>
+        Der Geburtstagsrechner ermöglicht (in seiner letzten Ausbaustufe) <b>jeden Tag Geburtstag zu feiern</b>. In unten stehender Webmaske können Geburtsdaten (nur das eigene, oder auch die von FreundInnen, KollegInnen, MitbewohnerInnen... oder auch
+        Jahrestage) eingegeben werden. Ein Klick auf "Geburtstage berechnen" gibt dann verschiedene Feiergründe aus, die ihr zusammen, oder alleine erreicht habt.
+    </p>
+    <div>
+        <form action="eingetragen.php" method="post">
+            <p><input type="text" name="subj_name">Name</p>
+            <p><input type="date" name="subj-datum">Datum</p>
+            <p><button type="submit">Geburtstag berechnen</button>
+        </form>
+    </div>-->
+</main>
+
+<?php 
+require "footer.php";
+?>
